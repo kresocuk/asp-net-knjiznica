0007fbecdfaf85565207a392be7f28caf77989de
kresocuk <kresocuk17@gmail.com>
2021-06-16 18:17:31 +0200

    lib

---
+++ LibraryData/Migrations/20210411215624_test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LibraryCardId",
+                table: "Patrons",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patrons_LibraryCardId",
+                table: "Patrons",
+                column: "LibraryCardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Patrons_LibraryCards_LibraryCardId",
+                table: "Patrons",
+                column: "LibraryCardId",
+                principalTable: "LibraryCards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Patrons_LibraryCards_LibraryCardId",
+                table: "Patrons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patrons_LibraryCardId",
+                table: "Patrons");
+
+            migrationBuilder.DropColumn(
+                name: "LibraryCardId",
+                table: "Patrons");
+        }
+    }
+}
---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id{ get; set; }
+        public string  FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress{ get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+
+        [Required]
+        [Display(Name = "Library Card")]
+        public LibraryCard LibraryCard { get; set; }
+        public LibraryBranch HomeLibraryBranch { get; set; }
+    }
+}
